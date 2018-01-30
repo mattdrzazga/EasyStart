76e3d112753e95449113f62e22660f605219cc12
Mateusz Drzazga <matt.drzazga@gmail.com>
2018-01-30 19:42:48 +0100

    Add StrictMode.

--- app/src/main/java/com/easystart/app/EasyStart.java
+++ app/src/main/java/com/easystart/app/EasyStart.java
@@ -1,13 +1,27 @@
 package com.easystart.app;
 
+import android.os.StrictMode;
+import com.easystart.BuildConfig;
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
-
 public class EasyStart extends DaggerApplication {
 
-    @Override
-    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+    @Override public void onCreate() {
+        super.onCreate();
+        initStrictMode();
+    }
+
+    private void initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                    new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+            StrictMode.setVmPolicy(
+                    new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+        }
+    }
+
+    @Override protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.builder().create(this);
     }
 }
